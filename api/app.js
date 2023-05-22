7932814d6087f0bdc82da9a3f7b7c93e6a6fa869
Djosef0 <youssefbensaid827@gmail.com>
2023-05-22 19:47:31 +0100

    add function add emploi

--- api/app.js
+++ api/app.js
@@ -1,28 +1,22 @@
 import  express  from "express";
 import User from "./routers/Users.js";
-// import  Produit  from "./routers/Produit.js";
 import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import fileUpload from "express-fileupload";
-// import  Msg  from "./routers/Msg.js";
-// import Payment from "./routers/Payment.js";
+import fileUpload from "express-fileupload";
+import Emploi from "./routers/Emplois.js";
 
 export const app = express();
 
 
 app.use(express.json());
-// app.use(cookieParser());
 app.use(express.urlencoded({extended : true}));
 app.use(cors());
-// app.use(
-//     fileUpload({
-//       limits: { fileSize: 50 * 1024 * 1024 },
-//       useTempFiles: true,
-//     })
-//   );
+app.use(
+    fileUpload({
+      limits: { fileSize: 50 * 1024 * 1024 },
+      useTempFiles: true,
+    })
+  );
  
 
 app.use("/api/users" , User);
-// app.use("/api/produits" , Produit);
-// app.use("/api/msg" , Msg);
-// app.use("/api/payment" , Payment);
+app.use("/api/emploi" , Emploi);
--- api/controllers/news.js
+++
@@ -1,8 +0,0 @@
-export const news = (req , res)=>{
-    try{
-        const content = req.body.content;
-        // const newContent = new new;
-    }catch(err){
-            res.status(500).json(err)
-    }
-}
